a93af9898ad29923256e3355f767e9b94f107f44
EmiSchaufeld <ebs338@nyu.edu>
2019-11-30 19:10:49 -0500

    Initial push of the unity project

---
+++ Last Night in Hamilton/Assets/Scripts/ChoiceManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public enum OlderKidsCheckpoint
+{
+    checkpointZero,
+    checkpointOne,
+    checkpointTwo,
+    checkpointThree
+}
+
+
+public class ChoiceManager : MonoBehaviour
+{
+
+    //public 
+    public Flowchart mainFlowchart;
+
+    public OlderKidsCheckpoint currentOlderKidsCheckpoint = OlderKidsCheckpoint.checkpointZero;
+    public Block olderKidController = new Block();
+    //int olderKidsNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //olderKidController.
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentOlderKidsCheckpoint != (OlderKidsCheckpoint)mainFlowchart.GetIntegerVariable("olderKids")) {
+
+            currentOlderKidsCheckpoint = (OlderKidsCheckpoint)mainFlowchart.GetIntegerVariable("olderKids");
+
+            switch (currentOlderKidsCheckpoint)
+            {
+                case OlderKidsCheckpoint.checkpointZero: Debug.Log("You haven't talked yet"); break;
+                case OlderKidsCheckpoint.checkpointOne: Debug.Log("You chose choice 1"); break;
+            }
+
+        }
+
+         
+
+                // Do something
+        }
+
+    /*
+    public void StoryPoint(){
+
+        switch(story){
+
+            case OlderKids:
+
+                if(olderCheckpoint == 0){ mainFlowchart etc etc }
+                if(olderCheckpoint == 1){ mainFlowchart etc etc }
+
+            break;
+        }
+
+    }
+
+
+
+
+
+    */
+
+    public void OlderKidsStoryPoint()
+    {
+        switch(currentOlderKidsCheckpoint)
+    {
+            case OlderKidsCheckpoint.checkpointZero: mainFlowchart.ExecuteBlock("Older Crowd 1"); break;
+            case OlderKidsCheckpoint.checkpointOne: mainFlowchart.ExecuteBlock("Older Crowd 2"); break;
+        }
+    }
+}
+
+
---
+++ Last Night in Hamilton/Assets/Scripts/Clock.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Clock : MonoBehaviour
+{
+    public int minutes;
+    public int hours;
+    public string ampm;
+    public Flowchart mainFlowchart; 
+
+  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string minutesString = minutes.ToString();
+        string hoursString = hours.ToString();
+
+        if (minutes == 60) {
+            minutes = 0;
+            hours++;
+        }
+      
+         if(minutes < 10){
+            minutesString = "0" + minutesString;
+         }
+
+       mainFlowchart.SetIntegerVariable("intMinutes", minutes);
+       mainFlowchart.SetIntegerVariable("intHours", hours);
+
+       mainFlowchart.SetStringVariable("stringMinutes", minutesString);
+       mainFlowchart.SetStringVariable("stringHours", hoursString);
+         
+    }
+    public void AddMinuteValue(int addedMinutes){
+        minutes += addedMinutes;
+        //call method in fungus to add time in conversations
+
+    }
+
+    
+}
